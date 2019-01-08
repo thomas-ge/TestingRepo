c8ef9d92ffb829eeea95be65214e72cd04df292e
Thomas Theux <t.grikschas@gmail.com>
2019-01-08 14:04:43 +0100

    Built basic interactive skilltree

---
+++ Test/Assets/Scripts/SkillTreeUIHandler.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Rewired;
+
+public class SkillTreeUIHandler : MonoBehaviour {
+
+	public GameObject buttonParent;
+	public Image cursor;
+
+	public List<Image> buttonArr;
+	public List<int> skillUpgradeCurrent;
+	private List<int> skillUpgradeMax = new List<int>(new int[] {
+		5, 5, 5, 1, 1, 1, 5, 1,
+		5, 5, 1,
+		1, 1, 1, 1, 1, 1, 1, 1,
+		5, 4, 4, 5, 1, 5, 3, 4,
+		1, 1, 1, 4, 1, 1
+	});
+
+	private int currentIndex = 15;
+
+
+	private void Awake() {
+		// Save all skill buttons in an array
+		for (int i = 0; i < buttonParent.transform.childCount; i++) {
+			buttonArr.Add(buttonParent.transform.GetChild(i).GetComponent<Image>());
+			skillUpgradeCurrent.Add(0);
+		}
+		
+		// This "skill" is already activated – because it's not really a skill but the class the player chose
+		skillUpgradeCurrent[currentIndex] = 1;
+		DisplayActivations();
+	}
+
+
+	private void Update() {
+		GetInput();
+
+		DisplayCursor();
+
+	}
+
+
+	private void GetInput() {
+		if (ReInput.players.GetPlayer(0).GetButtonDown("Up")) {
+			currentIndex = buttonArr[currentIndex].GetComponent<ButtonNav>().navUp.GetComponent<ButtonNav>().indexID;
+		}
+
+		if (ReInput.players.GetPlayer(0).GetButtonDown("Down")) {
+			currentIndex = buttonArr[currentIndex].GetComponent<ButtonNav>().navDown.GetComponent<ButtonNav>().indexID;
+		}
+
+		if (ReInput.players.GetPlayer(0).GetButtonDown("Left")) {
+			currentIndex = buttonArr[currentIndex].GetComponent<ButtonNav>().navLeft.GetComponent<ButtonNav>().indexID;
+		}
+
+		if (ReInput.players.GetPlayer(0).GetButtonDown("Right")) {
+			currentIndex = buttonArr[currentIndex].GetComponent<ButtonNav>().navRight.GetComponent<ButtonNav>().indexID;
+		}
+
+		if (ReInput.players.GetPlayer(0).GetButtonDown("X")) {
+			ActivateSkill();
+			DisplayActivations();
+		}
+	}
+
+
+	private void DisplayCursor() {
+		cursor.transform.position = buttonArr[currentIndex].transform.position;
+	}
+
+
+	private void ActivateSkill() {
+		if (skillUpgradeCurrent[currentIndex] < skillUpgradeMax[currentIndex]) {
+			skillUpgradeCurrent[currentIndex]++;
+		} else {
+			print("Already activated!");
+		}
+	}
+
+
+	private void DisplayActivations() {
+		if (skillUpgradeCurrent[currentIndex] == skillUpgradeMax[currentIndex]) {
+			buttonArr[currentIndex].GetComponent<Image>().color = new Color32(255, 255, 225, 255);
+			print("Skill learned!");
+		}
+	}
+
+}
---
+++ Test/Assets/Scripts/Twigs.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Twigs : MonoBehaviour {
+
+	public Slider twigsSlider;
+
+	private bool isIncreasing = false;
+
+	[Range(0, 100)]
+	public float currentValue = 0;
+	private float moveSpeed = 80.0f;
+
+
+	private void Update() {
+		if (isIncreasing) {
+			IncreaseValue();
+		} else {
+			DecreaseValue();
+		}
+
+		twigsSlider.value = currentValue;
+	}
+
+	
+	private void IncreaseValue() {
+		currentValue += moveSpeed * Time.deltaTime;
+
+		if (currentValue >= 100) {
+			isIncreasing = false;
+		}
+	}
+
+	
+	private void DecreaseValue() {
+		currentValue -= moveSpeed * Time.deltaTime;
+
+		if (currentValue <= 0) {
+			isIncreasing = true;
+		}
+	}
+}
