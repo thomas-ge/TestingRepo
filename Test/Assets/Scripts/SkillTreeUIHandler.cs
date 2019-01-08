bb749bd64f6cde2d4e1e7fa14aeabb460bae7521
Thomas Theux <t.grikschas@gmail.com>
2019-01-08 19:47:24 +0100

    Implemented activating other skills and limit activation to enabled skills

--- Test/Assets/Scripts/SkillTreeUIHandler.cs
+++ Test/Assets/Scripts/SkillTreeUIHandler.cs
@@ -26,11 +26,22 @@ public class SkillTreeUIHandler : MonoBehaviour {
 		// Save all skill buttons in an array
 		for (int i = 0; i < buttonParent.transform.childCount; i++) {
 			buttonArr.Add(buttonParent.transform.GetChild(i).GetComponent<Image>());
-			skillUpgradeCurrent.Add(0);
+			skillUpgradeCurrent.Add(-1);
 		}
+
+		// Initially enable skills that can be learned
+		for (int i = 0; i < 8; i++) {
+			skillUpgradeCurrent[i] = 0;
+		}
+		skillUpgradeCurrent[9] = 0;
+		for (int j = 11; j < 19; j++) {
+			skillUpgradeCurrent[j] = 0;
+		}
+		skillUpgradeCurrent[23] = 0;
 		
 		// This "skill" is already activated – because it's not really a skill but the class the player chose
 		skillUpgradeCurrent[currentIndex] = 1;
+
 		DisplayActivations();
 	}
 
@@ -39,7 +50,6 @@ public class SkillTreeUIHandler : MonoBehaviour {
 		GetInput();
 
 		DisplayCursor();
-
 	}
 
 
@@ -62,7 +72,6 @@ public class SkillTreeUIHandler : MonoBehaviour {
 
 		if (ReInput.players.GetPlayer(0).GetButtonDown("X")) {
 			ActivateSkill();
-			DisplayActivations();
 		}
 	}
 
@@ -73,18 +82,137 @@ public class SkillTreeUIHandler : MonoBehaviour {
 
 
 	private void ActivateSkill() {
-		if (skillUpgradeCurrent[currentIndex] < skillUpgradeMax[currentIndex]) {
-			skillUpgradeCurrent[currentIndex]++;
+		if (skillUpgradeCurrent[currentIndex] >= 0) {
+			if (skillUpgradeCurrent[currentIndex] < skillUpgradeMax[currentIndex]) {
+				skillUpgradeCurrent[currentIndex]++;
+
+				ActivateNext();
+				DisplayActivations();
+			} else {
+				print("Already activated!");
+			}
 		} else {
-			print("Already activated!");
+			print("This skill is locked");
+		}
+	}
+
+
+	private void ActivateNext() {
+		// Activate the next button in line if it's predecessor is enbled
+		if (skillUpgradeCurrent[3] == skillUpgradeMax[3] || skillUpgradeCurrent[4] == skillUpgradeMax[4] || skillUpgradeCurrent[5] == skillUpgradeMax[5]) {
+			if (skillUpgradeCurrent[8] < 0) {
+				skillUpgradeCurrent[8] = 0;
+			}
+		}
+
+		if (skillUpgradeCurrent[7] == skillUpgradeMax[7] && skillUpgradeCurrent[10] < 0) {
+			skillUpgradeCurrent[10] = 0;
+		}
+
+		for (int i = 11; i < 14; i++) {
+			if (skillUpgradeCurrent[i] == skillUpgradeMax[i] && skillUpgradeCurrent[i+8] < 0) {
+				skillUpgradeCurrent[i+8] = 0;
+			}
+		}
+
+		if (skillUpgradeCurrent[14] == skillUpgradeMax[14] && skillUpgradeCurrent[22] < 0) {
+			skillUpgradeCurrent[22] = 0;
+		}
+
+		if (skillUpgradeCurrent[16] == skillUpgradeMax[16] && skillUpgradeCurrent[24] < 0) {
+			skillUpgradeCurrent[24] = 0;
+		}
+
+		for (int j = 17; j < 19; j++) {
+			if (skillUpgradeCurrent[j] == skillUpgradeMax[j] && skillUpgradeCurrent[j+8] < 0) {
+				skillUpgradeCurrent[j+8] = 0;
+			}
+		}
+
+		if (skillUpgradeCurrent[23] == skillUpgradeMax[23] && skillUpgradeCurrent[30] < 0) {
+			skillUpgradeCurrent[30] = 0;
+		}
+
+		// Activate the next button in line if it's predecessor is complete
+		for (int k = 19; k < 22; k++) {
+			if (skillUpgradeCurrent[k] == skillUpgradeMax[k] && skillUpgradeCurrent[k+8] < 0) {
+				skillUpgradeCurrent[k+8] = 0;
+			}
+		}
+
+		for (int l = 25; l < 27; l++) {
+			if (skillUpgradeCurrent[l] == skillUpgradeMax[l] && skillUpgradeCurrent[l+6] < 0) {
+				skillUpgradeCurrent[l+6] = 0;
+			}
 		}
 	}
 
 
 	private void DisplayActivations() {
+		// Show the current level of activation
+		switch(currentIndex) {
+			case 0:
+			case 1:
+			case 2:
+			case 6:
+			case 8:
+			case 9:
+			case 19:
+			case 20:
+			case 21:
+			case 22:
+			case 24:
+			case 25:
+			case 26:
+			case 30:
+				buttonArr[currentIndex].transform.GetChild(1).GetComponent<Text>().text = skillUpgradeCurrent[currentIndex] + "/" + skillUpgradeMax[currentIndex];
+				break;
+			
+			// Lock the other skills when activating these
+			case 3:
+				skillUpgradeCurrent[4] = -2;
+				skillUpgradeCurrent[5] = -2;
+				break;
+
+			case 4:
+				skillUpgradeCurrent[3] = -2;
+				skillUpgradeCurrent[5] = -2;
+				break;
+
+			case 5:
+				skillUpgradeCurrent[3] = -2;
+				skillUpgradeCurrent[4] = -2;
+				break;
+
+			case 14:
+				skillUpgradeCurrent[16] = -2;
+				skillUpgradeCurrent[24] = -2;
+				break;
+
+			case 16:
+				skillUpgradeCurrent[14] = -2;
+				skillUpgradeCurrent[22] = -2;
+				break;
+		}
+
+		// Enable all skill buttons that have a 0 – aka make it non-transparent
+		for (int k = 0; k < buttonArr.Count; k++) {
+			if (skillUpgradeCurrent[k] == -2) {
+				buttonArr[k].GetComponent<Image>().color = new Color32(255, 255, 225, 30);
+			}
+			
+			if (skillUpgradeCurrent[k] == 0) {
+				buttonArr[k].GetComponent<Image>().color = new Color32(255, 255, 225, 255);
+			}
+
+			if (skillUpgradeCurrent[k] == 1) {
+				buttonArr[k].GetComponent<Image>().color = new Color32(255, 255, 225, 255);
+			}
+		}
+
+		// Set the selected skill to "done"
 		if (skillUpgradeCurrent[currentIndex] == skillUpgradeMax[currentIndex]) {
-			buttonArr[currentIndex].GetComponent<Image>().color = new Color32(255, 255, 225, 255);
-			print("Skill learned!");
+			buttonArr[currentIndex].transform.GetChild(0).GetComponent<Image>().color = new Color32(255, 0, 0, 255);
 		}
 	}
 
